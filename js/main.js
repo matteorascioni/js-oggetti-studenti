ec8460491396d14d03dace48942adf535ba7d595
Matteo <Rascioni>
2020-10-30 18:22:31 +0100

    Completato!

--- js/main.js
+++ js/main.js
@@ -15,45 +15,53 @@ $(document).ready(function() {
     /**
      * Array di oggetti studenti
      */
-    //  var arrayStudenti = [
-    //      { 
-    //         nome: 'Matteo',
-    //         cognome: 'Rascioni',
-    //         età: 23,
-    //      },
-    //      { 
-    //         nome: 'Paolo',
-    //         cognome: 'Duzioni',
-    //         età: 30,
-    //     },
-    //     { 
-    //         nome: 'Fabio',
-    //         cognome: 'Giorgini',
-    //         età: 40,
-    //     },
-    //     { 
-    //         nome: 'Marco',
-    //         cognome: 'Rossi',
-    //         età: 30,
-    //     },
-    //     { 
-    //         nome: 'Matteo',
-    //         cognome: 'Rossi',
-    //         età: 50,
-    //     },
-    //  ]
+     var arrayStudenti = [
+        studente = { 
+            nome: 'Matteo',
+            cognome: 'Rascioni',
+            età: 23,
+         },
+        studente = { 
+            nome: 'Paolo',
+            cognome: 'Duzioni',
+            età: 30,
+        },
+        studente = { 
+            nome: 'Fabio',
+            cognome: 'Giorgini',
+            età: 40,
+        },
+        studente = { 
+            nome: 'Marco',
+            cognome: 'Rossi',
+            età: 30,
+        },
+        studente = { 
+            nome: 'Matteo',
+            cognome: 'Rossi',
+            età: 50,
+        },
+     ]
 
-    //  for (var i = 0; i < arrayStudenti.length; i++) {
-    //     console.log('Nome: ' + arrayStudenti[i].nome, 'Cognome: ' + arrayStudenti[i].cognome, 'Età: ' + arrayStudenti[i].età  );
-    //  }
-
-
-
-
+     for (var i = 0; i < arrayStudenti.length; i++) {
+        console.log('Nome: ' + arrayStudenti[i].nome, 'Cognome: ' + arrayStudenti[i].cognome, 'Età: ' + arrayStudenti[i].età  );
+     }
 
     /**
      * 3 promp per l'utente e possibilità di aggiungere oggetti all'array 
      */
+        studenteAdd = {
+            nome: prompt('Inserisci il nome da aggiungere'), 
+            cognome: prompt('Inserisci il cognome da aggiungere'),
+            età: prompt('Inserisci l\' età'),
+        };
+    
+        arrayStudenti.push(studenteAdd);
+        console.log('Nome: ' + arrayStudenti[i].nome, 'Cognome: ' + arrayStudenti[i].cognome, 'Età: ' + arrayStudenti[i].età  );
+
+     
+
+    
 
 
 
