432176d6b76dca6b43a4c8e55dc315f59941c825
Matteo <Rascioni>
2020-10-30 16:09:44 +0100

    33.3% completato

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    /**
+    * Creazione oggetto studente (nome, cognome,età)
+    */
+    var studenteSingolo = {
+            nome: 'Matteo',
+            cognome: 'Rascioni',
+            Età: 23,
+        } 
+    console.log(studenteSingolo);
+
+
+
+
+    /**
+     * Array di oggetti studenti
+     */
+
+
+
+
+
+    /**
+     * 3 promp per l'utente e possibilità di aggiungere oggetti all'array 
+     */
+
+
+
+
+
+
+
+
+
+})
