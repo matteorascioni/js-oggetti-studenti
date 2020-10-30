0a773908fcbe38c79a86df6c943a1d47a5fd9550
Matteo <Rascioni>
2020-10-30 16:33:12 +0100

    70%

--- js/main.js
+++ js/main.js
@@ -2,19 +2,50 @@ $(document).ready(function() {
     /**
     * Creazione oggetto studente (nome, cognome,età)
     */
-    var studenteSingolo = {
-            nome: 'Matteo',
-            cognome: 'Rascioni',
-            Età: 23,
-        } 
-    console.log(studenteSingolo);
+    // var studenteSingolo = {
+    //         nome: 'Matteo',
+    //         cognome: 'Rascioni',
+    //         Età: 23,
+    //     } 
 
-
-
+    //     for (var key in studenteSingolo) {
+    //         console.log(studenteSingolo[key]);
+    //     }
 
     /**
      * Array di oggetti studenti
      */
+    //  var arrayStudenti = [
+    //      { 
+    //         nome: 'Matteo',
+    //         cognome: 'Rascioni',
+    //         età: 23,
+    //      },
+    //      { 
+    //         nome: 'Paolo',
+    //         cognome: 'Duzioni',
+    //         età: 30,
+    //     },
+    //     { 
+    //         nome: 'Fabio',
+    //         cognome: 'Giorgini',
+    //         età: 40,
+    //     },
+    //     { 
+    //         nome: 'Marco',
+    //         cognome: 'Rossi',
+    //         età: 30,
+    //     },
+    //     { 
+    //         nome: 'Matteo',
+    //         cognome: 'Rossi',
+    //         età: 50,
+    //     },
+    //  ]
+
+    //  for (var i = 0; i < arrayStudenti.length; i++) {
+    //     console.log('Nome: ' + arrayStudenti[i].nome, 'Cognome: ' + arrayStudenti[i].cognome, 'Età: ' + arrayStudenti[i].età  );
+    //  }
 
 
 
